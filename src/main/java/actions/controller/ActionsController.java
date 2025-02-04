2e14562591989121636bdc311d59cc50af0573d2
SUJINJEONG012 <peekaboo32@naver.com>
2025-02-04 13:23:29 +0900

    update

--- src/main/java/actions/controller/ActionsController.java
+++ src/main/java/actions/controller/ActionsController.java
@@ -1,13 +1,18 @@
 package actions.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class ActionsController {
 	
 	@GetMapping(value = "/api/actions")
 	public String actionsTest() {
+		log.info("");
 		return "app-with-actions version:0.0.1";
 	}
 	

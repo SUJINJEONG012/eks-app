38a12a55c5accd6f1f67cf78bc5175d4544e9a3c
SUJINJEONG012 <peekaboo32@naver.com>
2025-02-06 14:40:32 +0900

    파일 컨테이너에임시파일 저장되게 수정

--- src/main/java/actions/service/S3Service.java
+++ src/main/java/actions/service/S3Service.java
@@ -40,49 +40,110 @@ public class S3Service {
     private final String DIR_NAME = "/app/data"; 
 
     
-    // 파일 업로드
-	@Transactional
-	public void uploadS3File(MultipartFile file) throws Exception {
-		log.info("S3Service :uploadS3File " );
-		
-		if(file == null) {
-			throw new Exception("파일 전달 오류 발생");
-		}
-		
-		// 컨테이너 내부 볼륨 경로 사용
-		String filePath = DIR_NAME;
-		String attachmentOriginalFileName = file.getOriginalFilename();
-		
-		UUID uuid = UUID.randomUUID();
-		String attachmentFileName = uuid.toString()+ "_" + attachmentOriginalFileName;
-		Long attachmentFileSize= file.getSize();
-		
-		AttachmentFile attachmentFile = AttachmentFile.builder()
-				.filePath(filePath)
-				.attachmentOriginalFileName(attachmentOriginalFileName)
-				.attachmentFileName(attachmentFileName)
-				.attachmentFileSize(attachmentFileSize)
-				.build();
-		
-		Long fileNo = fileRepository.save(attachmentFile).getAttachmentFileNo();
-		
-		if(fileNo != null) {
-			// 물리적으로 파일을 저장할 경로
-			File uploadFile = new File(attachmentFile.getFilePath() + "/" + attachmentFileName);
-			file.transferTo(uploadFile);  // 컨테이너 내 /app/data 에 저장됨
-			
-			// S3 업로드
-	        String s3Key = "s3_data/" + uploadFile.getName();
-	        amazonS3.putObject(bucketName, s3Key, uploadFile);
+//    // 파일 업로드
+//	@Transactional
+//	public void uploadS3File(MultipartFile file) throws Exception {
+//		log.info("S3Service :uploadS3File " );
+//		
+//		if(file == null) {
+//			throw new Exception("파일 전달 오류 발생");
+//		}
+//		
+//		// 컨테이너 내부 볼륨 경로 사용
+//		String filePath = DIR_NAME;
+//		String attachmentOriginalFileName = file.getOriginalFilename();
+//		
+//		UUID uuid = UUID.randomUUID();
+//		String attachmentFileName = uuid.toString()+ "_" + attachmentOriginalFileName;
+//		Long attachmentFileSize= file.getSize();
+//		
+//		AttachmentFile attachmentFile = AttachmentFile.builder()
+//				.filePath(filePath)
+//				.attachmentOriginalFileName(attachmentOriginalFileName)
+//				.attachmentFileName(attachmentFileName)
+//				.attachmentFileSize(attachmentFileSize)
+//				.build();
+//		
+//		Long fileNo = fileRepository.save(attachmentFile).getAttachmentFileNo();
+//		
+//		if(fileNo != null) {
+//			// 물리적으로 파일을 저장할 경로
+//			File uploadFile = new File(attachmentFile.getFilePath() + "/" + attachmentFileName);
+//			file.transferTo(uploadFile);  // 컨테이너 내 /app/data 에 저장됨
+//			
+//			// S3 업로드
+//	        String s3Key = "s3_data/" + uploadFile.getName();
+//	        amazonS3.putObject(bucketName, s3Key, uploadFile);
+//
+////	        // S3에 저장되면 컨테이너 내 파일 삭제
+////	        if (uploadFile.exists()) {
+////	            uploadFile.delete();
+////	        }
+//	        
+//		}
+//		
+//	}
+    
+    @Transactional
+    public void uploadS3File(MultipartFile file) throws Exception {
+        log.info("S3Service :uploadS3File ");
+        
+        if (file == null) {
+            throw new Exception("파일 전달 오류 발생");
+        }
+        
+        // 로컬 파일 경로 설정
+        // DIR_NAME은 /app/data로 정의되어 있으므로, 이를 이용해 로컬 경로 설정
+        String filePath = DIR_NAME;  // "/app/data"로 설정된 디렉토리 경로 사용
+        
+        String attachmentOriginalFileName = file.getOriginalFilename();
+        UUID uuid = UUID.randomUUID();
+        String attachmentFileName = uuid.toString() + "_" + attachmentOriginalFileName;
+        Long attachmentFileSize = file.getSize();
+        
+        // AttachmentFile 객체 생성
+        AttachmentFile attachmentFile = AttachmentFile.builder()
+                .filePath(filePath)
+                .attachmentOriginalFileName(attachmentOriginalFileName)
+                .attachmentFileName(attachmentFileName)
+                .attachmentFileSize(attachmentFileSize)
+                .build();
+        
+        // 파일 정보를 DB에 저장
+        Long fileNo = fileRepository.save(attachmentFile).getAttachmentFileNo();
+        
+        if (fileNo != null) {
+            // 로컬에 파일 저장할 경로 생성
+            File dir = new File(filePath);
+            if (!dir.exists()) {
+                if (dir.mkdirs()) {
+                    log.info("디렉토리가 생성되었습니다: " + filePath);
+                } else {
+                    log.error("디렉토리 생성에 실패했습니다: " + filePath);
+                    throw new Exception("디렉토리 생성 실패");
+                }
+            }
+            
+            // 파일을 로컬에 저장
+            File uploadFile = new File(attachmentFile.getFilePath() + "/" + attachmentFileName);
+            file.transferTo(uploadFile);  // 파일 로컬에 저장
+            
+            log.info("파일이 로컬 임시 디렉토리에 저장되었습니다: " + uploadFile.getAbsolutePath());
+            
+            // S3에 파일 업로드
+            String s3Key = "s3_data/" + uploadFile.getName();  // S3에 저장될 경로
+            amazonS3.putObject(bucketName, s3Key, uploadFile);  // S3에 파일 업로드
+            
+            log.info("파일이 S3에 업로드되었습니다: " + s3Key);
+            
+            // S3에 파일이 업로드되면 로컬 파일 삭제 (주석처리한 부분을 사용 시 삭제 가능)
+//            if (uploadFile.exists()) {
+//                uploadFile.delete();
+//            }
+        }
+    }
 
-//	        // S3에 저장되면 컨테이너 내 파일 삭제
-//	        if (uploadFile.exists()) {
-//	            uploadFile.delete();
-//	        }
-	        
-		}
-		
-	}
+    
 	
 	// 파일 다운로드
 	@Transactional
